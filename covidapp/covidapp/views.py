9924564ac592572078b3c292b1a6805d586d133f
niampattni <niam.pattni@gmail.com>
2021-03-28 12:59:44 -0400

    reg/login

--- covidapp/covidapp/views.py
+++ covidapp/covidapp/views.py
@@ -53,7 +53,7 @@ class RiskView(APIView):
         sexprob = Sex.objects.get(age_group_id=agegroup, sex=user.sex).prob
         sexprob /= 100
         exp = request.data.get('exp')
-        exp = (0.25 * (1 - pow(0.953, expfac))) / (1 - 0.953)
+        exp = (0.25 * (1 - pow(0.953, exp))) / (1 - 0.953)
         expfac = Decimal(str(exp))
         locfac = Loc.objects.get(category=str(request.data.get('location'))).risk
         locfac = 1 / locfac
