62f752e03bf6697b25fb5f2a8c00e1a82eb9816c
niampattni <niam.pattni@gmail.com>
2021-03-28 13:17:48 -0400

    Risk calc testing

--- covidapp/covidapp/urls.py
+++ covidapp/covidapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import UserView, InfoView, RiskView
+from .views import UserView, InfoView, RiskView, LogoutView
 
 app_name = 'covidapp'
 urlpatterns = [
     path('login/', UserView.as_view(), name='login'),
     path('info/', InfoView.as_view(), name='info'),
     path('risk/', RiskView.as_view(), name='risk'),
+    path('logout/', LogoutView.as_view(), name='logout')
 ]
